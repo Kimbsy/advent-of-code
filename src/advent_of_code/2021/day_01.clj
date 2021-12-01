7821977af4ce3d56d8a24a21ca19a9c40365899b
David Kimber <david.kimber@thetrainline.com>
2021-12-01 09:42:15 +0000

    Add solution for 2021/day-01

--- src/advent_of_code/2021/day_01.clj
+++ src/advent_of_code/2021/day_01.clj
@@ -6,15 +6,27 @@
 (def input
   (line-seq (io/reader (io/resource "2021/day_01"))))
 
+(def test-input [199 200 208 210 200 207 240 269 260 263])
+
 (defn part-1
   []
-  )
+  (->> input
+       (map read-string)
+       (partition 2 1)
+       (filter (partial apply <))
+       count))
 
 (defn part-2
   []
-  )
+  (->> input
+       (map read-string)
+       (partition 3 1)
+       (map (partial reduce +))
+       (partition 2 1)
+       (filter (partial apply <))
+       count))
 
 (comment
-  (part-1) ;; =>
-  (part-2) ;; =>
+  (part-1) ;; => 1233
+  (part-2) ;; => 1275
   ,)
