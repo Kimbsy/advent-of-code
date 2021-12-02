d6f2fc521df5507dd376ad0461c7c7530bec7080
David Kimber <david.kimber@thetrainline.com>
2021-12-02 05:24:52 +0000

    Add solution for 2021/day-02

--- src/advent_of_code/2021/day_02.clj
+++ src/advent_of_code/2021/day_02.clj
@@ -4,18 +4,40 @@
             [clojure.set :as cset]
             [advent-of-code.util :as u]))
 
+(defn parse
+  [input]
+  (->> input
+       (map #(s/split % #" "))
+       (map #(update % 1 read-string))))
+
 (def input
-  (line-seq (io/reader (io/resource "2021/day_02"))))
+  (parse (line-seq (io/reader (io/resource "2021/day_02")))))
+
+(def test-input (parse ["forward 5" "down 5" "forward 8" "up 3" "down 8" "forward 2"]))
 
 (defn part-1
   []
-  )
+  (let [r (fn [acc [d n]]
+            (case d
+              "forward" (update acc :h + n)
+              "down" (update acc :d + n)
+              "up" (update acc :d - n)))
+        {:keys [h d]} (reduce r {:h 0 :d 0} input)]
+    (* h d)))
 
 (defn part-2
   []
-  )
+  (let [r (fn [{aim :a :as acc} [d n]]
+            (case d
+              "forward" (-> acc
+                            (update :h + n)
+                            (update :d + (* aim n)))
+              "down" (update acc :a + n)
+              "up" (update acc :a - n)))
+        {:keys [h d]} (reduce r {:h 0 :d 0 :a 0} input)]
+    (* h d)))
 
 (comment
-  (part-1) ;; =>
-  (part-2) ;; =>
+  (part-1) ;; => 1728414
+  (part-2) ;; => 1765720035
   ,)
