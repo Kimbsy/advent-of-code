bb2e6a73a40b16f156c8145ef0c3c2fc39e66e22
David Kimber <david.kimber@thetrainline.com>
2021-12-07 05:35:32 +0000

    Add solution for 2021/day-07

--- src/advent_of_code/2021/day_07.clj
+++ src/advent_of_code/2021/day_07.clj
@@ -5,17 +5,44 @@
             [advent-of-code.util :as u]))
 
 (def input
-  (line-seq (io/reader (io/resource "2021/day_07"))))
+  (map read-string (s/split (first (line-seq (io/reader (io/resource "2021/day_07")))) #",")))
+
+(def test-input [16 1 2 0 4 2 7 1 2 14])
+
+(defn abs
+  [n]
+  (max n (- n)))
 
 (defn part-1
   []
-  )
+  (let [low (apply min input)
+        high (apply max input)]
+    (first (sort (map (fn [t]
+                        (reduce + (map #(abs (- % t))
+                                       input)))
+                      (range low (inc high)))))))
+
+(def cost
+  (memoize
+   (fn [c t]
+     (reduce + (map inc (range (abs (- c t))))))))
 
 (defn part-2
   []
-  )
+  (let [low (apply min input)
+        high (apply max input)]
+    (first (sort (map (fn [t]
+                        (reduce + (map
+                                   (fn [c]
+                                     (cost c t))
+                                   input)))
+                      (range low (inc high))))))  )
 
 (comment
-  (part-1) ;; =>
-  (part-2) ;; =>
+  (part-1) ;; => 344605
+  (part-2) ;; => 93699985
   ,)
+
+
+;; refactoring check
+(= [(part-1) (part-2)] [344605 93699985])
