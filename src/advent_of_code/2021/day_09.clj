a347e34e2168aa7570ed7da6a227d61f25403795
David Kimber <david.kimber@thetrainline.com>
2021-12-09 17:29:47 +0000

    Refactor day-09
    Also add a bunch of utility functions for dealing with 2D grids

--- src/advent_of_code/2021/day_09.clj
+++ src/advent_of_code/2021/day_09.clj
@@ -5,67 +5,62 @@
             [advent-of-code.util :as u]))
 
 (def input
-  (mapv #(mapv u/char->int %) (line-seq (io/reader (io/resource "2021/day_09")))))
+  (line-seq (io/reader (io/resource "2021/day_09"))))
 
-(def test-input (mapv #(mapv u/char->int % ) ["2199943210" "3987894921" "9856789892" "8767896789" "9899965678"]))
+(def test-input ["2199943210" "3987894921" "9856789892" "8767896789" "9899965678"])
 
-(defn adj
-  [m [x y]]
-  (let [xs (filter #(<= 0 % (dec (count m))) [(dec x) x (inc x)])
-        ys (filter #(<= 0 % (dec (count (first m)))) [(dec y) y (inc y)])]
-    (remove nil?    (map #(get-in m %)
-                         (for [i xs
-                               j ys
-                               :when (not (and (= x i) (= y j)))]
-                           [i j])))))
+(defn parse-input
+  [in]
+  (mapv #(mapv u/char->int %) in))
 
-(defn card-adj-poss
-  [m [x y]]
-  (let [xs (filter #(<= 0 % (dec (count m))) [(dec x) x (inc x)])
-        ys (filter #(<= 0 % (dec (count (first m)))) [(dec y) y (inc y)])]
-    (for [i xs
-          j ys
-          :when (and (or (= x i) (= y j))
-                     (not (and (= x i) (= y j))))]
-      [i j])))
+(defn adj-vals
+  [m pos]
+  (map (partial u/grid-val m)
+       (u/adjacent-positions m pos)))
 
 (defn low?
   [m pos]
   (let [v (get-in m pos)]
-    (every? #(< v %) (adj m pos))))
+    (every? #(< v %) (adj-vals m pos))))
 
 (defn part-1
   []
-  (let [in input
-        poss (for [i (range (count in))
-                   j (range (count (first in)))]
-               [i j])]
+  (let [in (parse-input input)]
+    (->> (u/grid-positions in)
+         (filter #(not= 9 (u/grid-val in %)))
+         (filter #(low? in %))
+         (map (partial u/grid-val in))
+         (map inc)
+         (reduce +))))
 
-    (reduce + (map inc (map #(get-in in %) (filter #(low? in %) poss))))))
-
-(defn pos-val
+(defn non-9-cardinal-adjacent-positions
   [m pos]
-  (get-in m pos))
+  (remove #(= 9 (u/grid-val m %))
+          (u/cardinal-adjacent-positions m pos)))
 
 (defn expand
   [m poss]
-  (let [new (into #{} (concat (mapcat (fn [pos]
-                                        (remove #(= 9 (pos-val m %))
-                                                (card-adj-poss m pos)))
-                                      poss)
-                              poss))]
+  (let [new (->> poss
+                 (mapcat (partial non-9-cardinal-adjacent-positions m))
+                 (concat poss)
+                 (into #{}))]
     (if (= poss new)
       new
       (recur m new))))
 
 (defn part-2
   []
-  (let [in input
-        poss (for [i (range (count in))
-                   j (range (count (first in)))
-                   :when (not= 9 (pos-val in [i j]))]
-               [i j])]
-    (reduce * (take 3 (reverse (sort (map count (into #{} (map #(expand in #{%}) poss)))))))) )
+  (let [in (parse-input input)]
+    (->> (u/grid-positions in)
+         (filter #(not= 9 (u/grid-val in %)))
+         (filter #(low? in %))
+         (map #(expand in #{%}))
+         (into #{})
+         (map count)
+         sort
+         reverse
+         (take 3)
+         (reduce *))))
 
 (comment
   (part-1) ;; => 502
@@ -73,4 +68,5 @@
   ,)
 
 ;; refactoring check
+(= (part-1) 502)
 (= [(part-1) (part-2)] [502 1330560])
--- src/advent_of_code/util.clj
+++ src/advent_of_code/util.clj
@@ -10,10 +10,6 @@
   [n]
   (= [1 n] (divisors n)))
 
-(defn transpose
-  [m]
-  (apply map list m))
-
 (defn char->int
   [c]
   (Character/digit c 10))
@@ -21,3 +17,64 @@
 (defn abs
   [n]
   (max n (- n)))
+
+;;; Multidimensional grid stuff
+
+;; Note: x and y will be backwards, but it should be fine if we always
+;; ignore it. When printing we can use `transpose` to make it look
+;; right.
+
+(defn transpose
+  [m]
+  (apply map list m))
+
+(defn grid-of
+  "Create a grid of `v` whose dimensions are specified by dims"
+  [v & dims]
+  (reduce (fn [acc d]
+            (vec (repeat d acc)))
+          (vec (repeat (first dims) v))
+          (rest dims)))
+
+(defn grid-positions
+  "The list of position vectors in a 2D grid"
+  [grid]
+  (for [i (range (count grid))
+        j (range (count (first grid)))]
+    [i j]))
+
+(defn grid-val
+  "Get the value at a position in a multidimensional grid"
+  [grid pos]
+  (get-in grid pos))
+
+(defn bounded-adjacent-xys
+  "Get the adjacent row/column indices for a position on a 2D grid"
+  [grid [x y]]
+  [(filter #(<= 0 % (dec (count grid))) [(dec x) x (inc x)])
+   (filter #(<= 0 % (dec (count (first grid)))) [(dec y) y (inc y)])])
+
+(defn adjacent-positions
+  "Get the adjacent positions for a position on a 2D grid
+
+  This will return 3 positions for a corner position, 5 for an edge
+  position, or all 8 for a middle position"
+  [grid [x y :as pos]]
+  (let [[xs ys] (bounded-adjacent-xys grid pos)]
+    (for [i xs
+          j ys
+          :when (not (and (= x i) (= y j)))]
+      [i j])))
+
+(defn cardinal-adjacent-positions
+  "Get the cardinally adjacent positions for a position on a 2D grid
+
+  This will return 2 positions for a corner position, 3 for an edge
+  position, or all 4 for a middle position"
+  [grid [x y :as pos]]
+  (let [[xs ys] (bounded-adjacent-xys grid pos)]
+    (for [i xs
+          j ys
+          :when (and (or (= x i) (= y j))
+                     (not (and (= x i) (= y j))))]
+      [i j])))
