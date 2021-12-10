281ee96df18510e18f21c0019d78fa493e6ca10e
David Kimber <david.kimber@thetrainline.com>
2021-12-10 05:54:43 +0000

    Refactor results ns
    we want to be able to see the hh:mm:ss in the order function

--- src/advent_of_code/results.clj
+++ src/advent_of_code/results.clj
@@ -4,10 +4,14 @@
 
 (def results (json/parse-string (slurp "resources/2021/results")))
 
-(defn readable-time
+(defn readable-diff
   [seconds]
   (let [t (time/seconds->timespan seconds)]
     (str (time/timespan->hours t) "h " (time/timespan->minutes t) "m " (time/timespan->seconds t) "s")))
+
+(defn readable-ts
+  [ts]
+  (time/format (time/datetime (* 1000 ts)) "HH:mm:ss"))
 
 (defn order
   [day star]
@@ -18,12 +22,14 @@
        (map (juxt #(get %"name")
                   (fn [m]
                     (get-in m ["completion_day_level" (str day) (str star) "get_star_ts"]))))
+       (map (fn [[name ts]]
+              [name ts (readable-ts ts)]))
        (sort-by second)))
 
 (defn relative-order
   [day star]
   (let [order (order day star)
         best (second (first order))]
-    (map (fn [[name time]]
-           [name (readable-time (- time best))])
+    (map (fn [[name ts readable-ts]]
+           [name ts (readable-diff (- ts best))])
          order)))
