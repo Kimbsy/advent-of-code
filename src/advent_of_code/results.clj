617a262bb37ea4c6aca37daa50f0261a2e077260
David Kimber <david.kimber@thetrainline.com>
2021-12-17 15:54:26 +0000

    Update results ns to be able to create bar chart races with flourish

--- src/advent_of_code/results.clj
+++ src/advent_of_code/results.clj
@@ -1,5 +1,8 @@
 (ns advent-of-code.results
-  (:require [cheshire.core :as json]
+  (:require [advent-of-code.util :as u]
+            [cheshire.core :as json]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
             [simple-time.core :as time]))
 
 (def results (json/parse-string (slurp "resources/2021/results")))
@@ -19,7 +22,7 @@
        (map second)
        (filter (fn [m]
                  (get-in m ["completion_day_level" (str day) (str star)])))
-       (map (juxt #(get %"name")
+       (map (juxt #(get % "name")
                   (fn [m]
                     (get-in m ["completion_day_level" (str day) (str star) "get_star_ts"]))))
        (map (fn [[name ts]]
@@ -33,3 +36,64 @@
     (map (fn [[name ts readable-ts]]
            [name ts (readable-diff (- ts best))])
          order)))
+
+(defn add-score
+  [acc [day star]]
+  (->> (order day star)
+       (map first)
+       (map list (reverse (map inc (range (count acc)))))
+       (reduce (fn [acc [score player]]
+                 (update acc player + score))
+               acc)))
+
+(defn initial-leaderboard
+  []
+  (->> (get results "members")
+       (map second)
+       (map (fn [player-data]
+              (or (get player-data "name")
+                  (str "(anonymous user #" (get player-data "id") ")"))))
+       (map (fn [player] [player 0]))
+       (into {})))
+
+(def day-stars (for [d (map inc (range 25))
+                     s (map inc (range 2))]
+                 [d s]))
+
+(defn relevant-day-stars
+  [[day star]]
+  (take-while (fn [[d s]]
+                (or (< d day)
+                    (and (= d day)
+                         (<= s star))))
+              day-stars))
+
+(defn score-at
+  [day-star]
+  (->> (relevant-day-stars day-star)
+       (reduce add-score
+               (initial-leaderboard))
+       (sort-by second)
+       reverse))
+
+(defn progression
+  [day-star]
+  (->> (relevant-day-stars day-star)
+       (reduce (fn [acc ds]
+                 (conj acc (score-at ds)))
+               [])))
+
+(defn flourish-data
+  []
+  (let [progression-data (progression [25 2])
+        sorted-data (map #(sort-by first %) progression-data)
+        names (map list (map first (first sorted-data)))
+        numbers (map #(map second %) sorted-data)
+        table (map concat names (repeat ["" "" ""]) (u/transpose numbers))
+        header (concat ["names" "" "" ""] (map (fn [[d s]] (str d "-" s)) day-stars))]
+    (concat [header] table)))
+
+(defn write-data!
+  [data]
+  (with-open [w (io/writer "resources/2021/flourish.csv")]
+    (csv/write-csv w data)))
