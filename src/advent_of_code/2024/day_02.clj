22bfcc5ce71ace85f1cb4befec25c08cb4e474a6
Kimbsy <lordkimber@gmail.com>
2024-12-03 14:02:14 +0000

    day 2 and 3 2024

--- src/advent_of_code/2024/day_02.clj
+++ src/advent_of_code/2024/day_02.clj
@@ -8,24 +8,52 @@
 (def input
   (line-seq (io/reader (io/resource "2024/day_02"))))
 
-(def test-input [])
+(def test-input ["7 6 4 2 1"
+                 "1 2 7 8 9"
+                 "9 7 6 2 1"
+                 "1 3 2 4 5"
+                 "8 6 4 4 1"
+                 "1 3 6 7 9"])
 
 (defn parse-input
   [in]
-  in)
+  (map #(read-string (str "[" % "]"))
+       in))
+
+(defn safe?
+  [report]
+  (and (or (apply < report)
+           (apply > report))
+       (let [pairs (partition 2 1 report)]
+         (every? (fn [[a b]]
+                   (<= 1 (abs (- a b)) 3))
+                 pairs))))
 
 (defn part-1
   []
-  )
+  (let [reports (parse-input input)]
+    (count (filter safe? reports))))
+
+(defn get-options
+  [report]
+  (for [i (range (count report))]
+    (concat (take i report)
+            (drop (inc i) report))))
+
+(defn damped-safe?
+  [report]
+  (let [options (get-options report)]
+    (some safe? options)))
 
 (defn part-2
   []
-  )
+  (let [reports (parse-input input)]
+    (count (filter damped-safe? reports))))
 
 (comment
-  (part-1) ;; => 
-  (part-2) ;; => 
+  (part-1) ;; => 369
+  (part-2) ;; => 428
   ,)
 
 ;; refactoring check
-#_(= [(part-1) (part-2)] [])
+(= [(part-1) (part-2)] [369 428])
--- src/advent_of_code/2024/day_03.clj
+++ src/advent_of_code/2024/day_03.clj
@@ -6,9 +6,9 @@
             [advent-of-code.util :as u]))
 
 (def input
-  (line-seq (io/reader (io/resource "2024/day_03"))))
+  (slurp (io/reader (io/resource "2024/day_03"))))
 
-(def test-input [])
+(def test-input "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
 
 (defn parse-input
   [in]
@@ -16,16 +16,32 @@
 
 (defn part-1
   []
-  )
+  (->> input
+       (re-seq #"mul\((\d{1,3}),(\d{1,3})\)")
+       (map (partial drop 1))
+       (map #(map read-string %))
+       (map (partial apply *))
+       (reduce +)))
+
+(def test-input-2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
 
 (defn part-2
   []
+  (let [in (s/replace input #"(?s)don\'t\(\).*?do\(\)" "")]
+    (->> in
+         (re-seq #"mul\((\d{1,3}),(\d{1,3})\)")
+         (map (partial drop 1))
+         (map #(map read-string %))
+         (map (partial apply *))
+         (reduce +)))
+
+  
   )
 
 (comment
-  (part-1) ;; => 
-  (part-2) ;; => 
+  (part-1) ;; => 155955228
+  (part-2) ;; => 100189366
   ,)
 
 ;; refactoring check
-#_(= [(part-1) (part-2)] [])
+(= [(part-1) (part-2)] [155955228 100189366])
