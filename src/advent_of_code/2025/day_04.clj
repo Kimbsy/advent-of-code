a970b9171e9eef804b6864837f7aa13247217dce
Kimbsy <lordkimber@gmail.com>
2025-12-06 13:33:55 +0000

    day 4

--- src/advent_of_code/2025/day_04.clj
+++ src/advent_of_code/2025/day_04.clj
@@ -8,24 +8,63 @@
 (def input
   (line-seq (io/reader (io/resource "2025/day_04"))))
 
-(def test-input [])
+(def test-input
+  ["..@@.@@@@."
+   "@@@.@.@.@@"
+   "@@@@@.@.@@"
+   "@.@@@@..@."
+   "@@.@@@@.@@"
+   ".@@@@@@@.@"
+   ".@.@.@.@@@"
+   "@.@@@.@@@@"
+   ".@@@@@@@@."
+   "@.@.@@@.@."])
 
 (defn parse-input
   [in]
-  in)
+  (mapv vec in))
+
+(defn available
+  [in]
+  (->> in
+       u/grid-positions
+       ;;(take 3)
+       (filter #(#{\@} (u/grid-val in %)))
+       (map (fn [p] [p (u/adjacent-positions in p)]))
+       (map (fn [[p ps]]
+              [p (->> ps
+                      (map (partial u/grid-val in))
+                      (filter #{\@})
+                      count)]))
+       (filter #(< (second %) 4))))
 
 (defn part-1
   []
-  )
+  (let [in (parse-input input)]
+    (->> in
+         available
+         count)))
+
+(defn all-available
+  [in total]
+  (let [to-remove (available in)]
+    (if (empty? to-remove)
+      total
+      (let [updated (reduce (fn [acc [[x y] _]]
+                              (assoc-in acc [x y] \.))
+                            in
+                            to-remove)]
+        (all-available updated (+ total (count to-remove)))))))
 
 (defn part-2
   []
-  )
+  (let [in (parse-input input)]
+    (all-available in 0)))
 
 (comment
-  (part-1) ;; => 
+  (part-1) ;; => 1516
   (part-2) ;; => 
   ,)
 
 ;; refactoring check
-#_(= [(part-1) (part-2)] [])
+(= [(part-1) #_(part-2)] [1516])
